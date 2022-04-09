d681d7c3cf5e683b93ba5a6e65b49c7e08efa5c8
Silvestre08 <103322228+Silvestre08@users.noreply.github.com>
2022-04-09 16:22:53 +0100

    Add celestial object

---
+++ StarChart/Models/CelestialObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StarChart.Models
+{
+    public class CelestialObject
+    {
+        public int Id { get; }
+
+        [Required]
+        public string Name { get; }
+
+
+        public int? OrbitedObjectId { get; }
+
+        [NotMapped]
+        public List<CelestialObject> Satellite { get; }
+
+        public TimeSpan OrbitalPeriod { get; }
+    }
+}
